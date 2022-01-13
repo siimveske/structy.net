5c7bd4392d456fc08c75817f6b24066ae99b5348
Siim Veske <siim.veske@gmail.com>
2022-01-13 20:17:26 +0200

    include structy solution

--- graphs/shortest_path.py
+++ graphs/shortest_path.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 
 '''
 https://structy.net/problems/island-count
+https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=5976s
 '''
 
 grid1 = [
@@ -55,12 +56,6 @@ def build_graph(edges):
 
 def islandCount(grid):
     graph = build_graph(grid)
-    result = bfs(graph)
-    return result
-
-
-def bfs(graph):
-
     visited = set()
     island_count = 0
 
@@ -80,10 +75,48 @@ def bfs(graph):
     return island_count
 
 
+def explore(grid, row, col, visited):
+    rowInbound = 0 <= row <= len(grid)
+    colInboud = 0 <= col <= len(grid[0])
+
+    if not rowInbound or not colInboud:
+        return False
+    if grid[row][col] == 'W':
+        return False
+    if (row, col) in visited:
+        return False
+
+    visited.add((row, col))
+
+    explore(grid, row-1, col, visited)
+    explore(grid, row+1, col, visited)
+    explore(grid, row, col-1, visited)
+    explore(grid, row, col+1, visited)
+
+    return True
+
+
+def islandCount2(grid):
+    visited = set()
+    island_count = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if explore(grid, row, col, visited):
+                island_count += 1
+    return island_count
+
+
 if __name__ == '__main__':
 
     assert islandCount(grid1) == 3
     assert islandCount(grid2) == 4
     assert islandCount(grid3) == 1
     assert islandCount(grid4) == 0
+
+    assert islandCount2(grid1) == 3
+    assert islandCount2(grid2) == 4
+    assert islandCount2(grid3) == 1
+    assert islandCount2(grid4) == 0
+
     print('OK')
