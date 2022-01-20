ecbb8905c5562073ef964f0b1654f04efec6ec0c
Siim Veske <siim.veske@gmail.com>
2022-01-20 21:30:30 +0200

    solve sum-list

---
+++ linked-list/sum_list.py
@@ -0,0 +1,48 @@
+import unittest
+from node import Node
+
+
+def sum_list(head):
+    total = 0
+    current_node = head
+
+    while current_node:
+        total += current_node.val
+        current_node = current_node.next
+
+    return total
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node(2)
+        b = Node(8)
+        c = Node(3)
+        d = Node(-1)
+        e = Node(7)
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+
+        assert sum_list(a) == 19
+
+    def test_01(self):
+        x = Node(38)
+        y = Node(4)
+
+        x.next = y
+
+        assert sum_list(x) == 42
+
+    def test_02(self):
+        z = Node(100)
+        assert sum_list(z) == 100
+
+    def test_03(self):
+        assert sum_list(None) == 0
+
+
+if __name__ == "__main__":
+    unittest.main()
