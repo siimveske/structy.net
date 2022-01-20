7791a3d865b9411f4d8ba9e87604a80375f66a2e
Siim Veske <siim.veske@gmail.com>
2022-01-20 21:54:37 +0200

    solve node value

---
+++ linked-list/get_node_value.py
@@ -0,0 +1,75 @@
+import unittest
+from node import Node
+
+
+def get_node_value(head, index):
+    if head is None:
+        return None
+
+    position = 0
+    current_node = head
+    while position < index:
+        if current_node.next is None:
+            return None
+        current_node = current_node.next
+        position += 1
+
+    return current_node.val
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+
+        a.next = b
+        b.next = c
+        c.next = d
+
+        assert get_node_value(a, 2) == "c"
+
+    def test_01(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+
+        a.next = b
+        b.next = c
+        c.next = d
+
+        assert get_node_value(a, 3) == "d"
+
+    def test_02(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+
+        a.next = b
+        b.next = c
+        c.next = d
+
+        assert get_node_value(a, 7) == None
+
+    def test_03(self):
+        node1 = Node("banana")
+        node2 = Node("mango")
+
+        node1.next = node2
+
+        assert get_node_value(node1, 0) == "banana"
+
+    def test_04(self):
+        node1 = Node("banana")
+        node2 = Node("mango")
+
+        node1.next = node2
+
+        assert get_node_value(node1, 1) == "mango"
+
+
+if __name__ == "__main__":
+    unittest.main()
