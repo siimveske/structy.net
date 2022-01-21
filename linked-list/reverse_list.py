f1274ef1eaec6be85bbb2a68a1144118177157e1
Siim Veske <siim.veske@gmail.com>
2022-01-21 07:39:13 +0200

    update linked list tests

--- linked-list/reverse_list.py
+++ linked-list/reverse_list.py
@@ -1,5 +1,6 @@
 import unittest
 from node import Node
+from linked_list_values import linked_list_values
 
 
 def reverse_list(head):
@@ -29,7 +30,8 @@ class Test(unittest.TestCase):
         d.next = e
         e.next = f
 
-        assert reverse_list(a).val == 'f'
+        result = reverse_list(a)
+        assert linked_list_values(result) == ["f", "e", "d", "c", "b", "a"]
 
     def test_01(self):
         x = Node("x")
@@ -37,11 +39,13 @@ class Test(unittest.TestCase):
 
         x.next = y
 
-        assert reverse_list(x).val == 'y'
+        result = reverse_list(x)
+        assert linked_list_values(result) == ["y", "x"]
 
     def test_02(self):
         p = Node("p")
-        assert reverse_list(p).val == 'p'
+        result = reverse_list(p)
+        assert linked_list_values(result) == ["p"]
 
 
 if __name__ == "__main__":
