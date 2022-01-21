e5dfa352e49e43e6394274a7d38c8b69ed8d6d08
Siim Veske <siim.veske@gmail.com>
2022-01-21 12:56:42 +0200

    btree dfs

---
+++ btree/depth_first_values.py
@@ -0,0 +1,204 @@
+import unittest
+from node import Node
+
+
+def depth_first_values(root: Node):
+    if root is None:
+        return []
+
+    values = []
+    stack = [root]
+    while stack:
+        item = stack.pop()
+        values.append(item.val)
+        if item.right:
+            stack.append(item.right)
+        if item.left:
+            stack.append(item.left)
+
+    return values
+
+
+def rdepth_first_values(root: Node):
+    if root is None:
+        return []
+
+    lvalues = rdepth_first_values(root.left)
+    rvalues = rdepth_first_values(root.right)
+
+    return [root, *lvalues, *rvalues]
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        values = depth_first_values(a)
+        assert values == ["a", "b", "d", "e", "c", "f"]
+
+    def test_01(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        g = Node("g")
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+        #    /
+        #   g
+
+        values = depth_first_values(a)
+        assert values == ["a", "b", "d", "e", "g", "c", "f"]
+
+    def test_02(self):
+        a = Node("a")
+        #     a
+        values = depth_first_values(a)
+        assert values == ["a"]
+
+    def test_03(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        a.right = b
+        b.left = c
+        c.right = d
+        d.right = e
+
+        #      a
+        #       \
+        #        b
+        #       /
+        #      c
+        #       \
+        #        d
+        #         \
+        #          e
+
+        values = depth_first_values(a)
+        assert values == ["a", "b", "c", "d", "e"]
+
+    def test_04(self):
+        values = depth_first_values(None)
+        assert values == []
+
+
+def test_05(self):
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    values = rdepth_first_values(a)
+    assert values == ["a", "b", "d", "e", "c", "f"]
+
+
+def test_06(self):
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /
+    #   g
+
+    values = rdepth_first_values(a)
+    assert values == ["a", "b", "d", "e", "g", "c", "f"]
+
+
+def test_07(self):
+    a = Node("a")
+    #     a
+    values = rdepth_first_values(a)
+    assert values == ["a"]
+
+
+def test_08(self):
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    a.right = b
+    b.left = c
+    c.right = d
+    d.right = e
+
+    #      a
+    #       \
+    #        b
+    #       /
+    #      c
+    #       \
+    #        d
+    #         \
+    #          e
+
+    values = rdepth_first_values(a)
+    assert values == ["a", "b", "c", "d", "e"]
+
+
+def test_09(self):
+    values = rdepth_first_values(None)
+    assert values == []
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ btree/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
