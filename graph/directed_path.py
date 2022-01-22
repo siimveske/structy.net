072f37f77229ac170a41c5e6d0bf65d5589a44c9
Siim Veske <siim.veske@gmail.com>
2022-01-23 01:57:18 +0200

    refactor directed and undirected path solutions

---
+++ graph/directed_path.py
@@ -0,0 +1,146 @@
+import unittest
+
+
+def has_path(graph, src, dst):
+    """return a boolean indicating whether or not there
+    exists a directed path between the source and destination nodes"""
+    stack = [src]
+    while stack:
+        node = stack.pop()
+        if node == dst:
+            return True
+        stack += graph[node]
+    return False
+
+
+def has_path_rec(graph, src, dst):
+    """return a boolean indicating whether or not there
+    exists a directed path between the source and destination nodes"""
+    if src == dst:
+        return True
+    for node in graph[src]:
+        if has_path_rec(graph, node, dst):
+            return True
+    return False
+
+
+class Test(unittest.TestCase):
+    def test_01(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        assert has_path(graph, "f", "k") == True
+
+    def test_02(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        assert has_path(graph, "f", "j") == False
+
+    def test_03(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        has_path(graph, "i", "h") == True
+
+    def test_04(self):
+        graph = {
+            "v": ["x", "w"],
+            "w": [],
+            "x": [],
+            "y": ["z"],
+            "z": [],
+        }
+
+        assert has_path(graph, "v", "w") == True
+
+    def test_05(self):
+        graph = {
+            "v": ["x", "w"],
+            "w": [],
+            "x": [],
+            "y": ["z"],
+            "z": [],
+        }
+
+        assert has_path(graph, "v", "z") == False
+
+    def test_06(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        assert has_path_rec(graph, "f", "k") == True
+
+    def test_07(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        assert has_path_rec(graph, "f", "j") == False
+
+    def test_08(self):
+        graph = {
+            "f": ["g", "i"],
+            "g": ["h"],
+            "h": [],
+            "i": ["g", "k"],
+            "j": ["i"],
+            "k": [],
+        }
+
+        has_path_rec(graph, "i", "h") == True
+
+    def test_09(self):
+        graph = {
+            "v": ["x", "w"],
+            "w": [],
+            "x": [],
+            "y": ["z"],
+            "z": [],
+        }
+
+        assert has_path_rec(graph, "v", "w") == True
+
+    def test_10(self):
+        graph = {
+            "v": ["x", "w"],
+            "w": [],
+            "x": [],
+            "y": ["z"],
+            "z": [],
+        }
+
+        assert has_path_rec(graph, "v", "z") == False
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ graph/undirected_path.py
@@ -0,0 +1,295 @@
+import unittest
+from collections import defaultdict
+
+
+def undirected_path(edges, node_A, node_B):
+    graph = build_graph(edges)
+    return has_path(graph, node_A, node_B)
+
+
+def undirected_path_rec(edges, node_A, node_B):
+    graph = build_graph(edges)
+    return has_path_rec(graph, node_A, node_B, set())
+
+
+def build_graph(edges):
+    graph = defaultdict(list)
+    for i, j in edges:
+        graph[i].append(j)
+        graph[j].append(i)
+    return graph
+
+
+def has_path(graph, start, end):
+    """The function returns a boolean indicating whether
+    or not there exists a path between node_A and node_B"""
+
+    stack = [start]
+    visited = set()
+
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+        if node == end:
+            return True
+        visited.add(node)
+        stack += graph[node]
+
+    return False
+
+
+def has_path_rec(graph, start, end, visited):
+    """The function returns a boolean indicating whether
+    or not there exists a path between node_A and node_B"""
+
+    if start == end:
+        return True
+
+    visited.add(start)
+
+    for node in graph[start]:
+        if node in visited:
+            continue
+        if has_path_rec(graph, node, end, visited):
+            return True
+
+    return False
+
+
+class Test(unittest.TestCase):
+    def test_01(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path(edges, 'j', 'm') == True
+
+    def test_02(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path(edges, 'm', 'j') == True
+
+    def test_03(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path(edges, 'l', 'j') == True
+
+    def test_04(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path(edges, 'k', 'o') == False
+
+    def test_05(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path(edges, 'i', 'o') == False
+
+    def test_06(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path(edges, 'a', 'b') == True
+
+    def test_07(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path(edges, 'a', 'c') == True
+
+    def test_08(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path(edges, 'r', 't') == True
+
+    def test_09(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path(edges, 'r', 'b') == False
+
+    def test_10(self):
+        edges = [
+            ('s', 'r'),
+            ('t', 'q'),
+            ('q', 'r'),
+        ]
+
+        undirected_path(edges, 'r', 't') == True
+
+    def test_11(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path_rec(edges, 'j', 'm') == True
+
+    def test_12(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path_rec(edges, 'm', 'j') == True
+
+    def test_13(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path_rec(edges, 'l', 'j') == True
+
+    def test_14(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path_rec(edges, 'k', 'o') == False
+
+    def test_15(self):
+        edges = [
+            ('i', 'j'),
+            ('k', 'i'),
+            ('m', 'k'),
+            ('k', 'l'),
+            ('o', 'n')
+        ]
+
+        assert undirected_path_rec(edges, 'i', 'o') == False
+
+    def test_16(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path_rec(edges, 'a', 'b') == True
+
+    def test_17(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path_rec(edges, 'a', 'c') == True
+
+    def test_18(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path_rec(edges, 'r', 't') == True
+
+    def test_19(self):
+        edges = [
+            ('b', 'a'),
+            ('c', 'a'),
+            ('b', 'c'),
+            ('q', 'r'),
+            ('q', 's'),
+            ('q', 'u'),
+            ('q', 't'),
+        ]
+
+        undirected_path_rec(edges, 'r', 'b') == False
+
+    def test_20(self):
+        edges = [
+            ('s', 'r'),
+            ('t', 'q'),
+            ('q', 'r'),
+        ]
+
+        undirected_path_rec(edges, 'r', 't') == True
+
+
+if __name__ == '__main__':
+    unittest.main()
