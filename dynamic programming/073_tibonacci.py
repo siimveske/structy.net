1736a9fc78161870bf1e9c190021d1c6e08c7a50
Siim Veske <siim.veske@gmail.com>
2022-01-23 19:55:58 +0200

    solve tribonacci

--- dynamic programming/073_tibonacci.py
+++ dynamic programming/073_tibonacci.py
@@ -1,5 +1,14 @@
 import unittest
 
+"""
+tribonacci
+Write a function tribonacci that takes in a number argument, n, and returns the n-th number of the Tribonacci sequence.
+The 0-th and 1-st numbers of the sequence are both 0.
+The 2-nd number of the sequence is 1.
+To generate further numbers of the sequence, calculate the sum of previous three numbers.
+Solve this recursively.
+"""
+
 
 def tribonacci(n):
     solutions = {0: 0, 1: 0, 2: 1}
