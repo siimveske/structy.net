40ed9f7d876f7252235917ba261556f5358d1d93
Siim Veske <siim.veske@gmail.com>
2022-01-23 13:05:40 +0200

    mpve shortest path tests into unittest class

--- graph/shortest_path.py
+++ graph/shortest_path.py
@@ -5,6 +5,7 @@ https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=5043s
 
 from collections import deque
 from collections import defaultdict
+import unittest
 
 
 def build_graph(edges):
@@ -28,91 +29,100 @@ def shortest_path(edges, node_A, node_B):
         for neighbor in graph[node]:
             if neighbor not in visited:
                 visited.add(neighbor)
-                queue.append((neighbor, distance+1))
+                queue.append((neighbor, distance + 1))
 
     return -1
 
 
+class Test(unittest.TestCase):
+    def test_00(self):
+        edges = [
+            ['w', 'x'],
+            ['x', 'y'],
+            ['z', 'y'],
+            ['z', 'v'],
+            ['w', 'v']
+        ]
+        assert shortest_path(edges, 'w', 'z') == 2
+
+    def test_01(self):
+        edges = [
+            ['w', 'x'],
+            ['x', 'y'],
+            ['z', 'y'],
+            ['z', 'v'],
+            ['w', 'v']
+        ]
+        assert shortest_path(edges, 'y', 'x') == 1
+
+    def test_02(self):
+        edges = [
+            ['a', 'c'],
+            ['a', 'b'],
+            ['c', 'b'],
+            ['c', 'd'],
+            ['b', 'd'],
+            ['e', 'd'],
+            ['g', 'f']
+        ]
+        assert shortest_path(edges, 'a', 'e') == 3
+
+    def test_03(self):
+        edges = [
+            ['a', 'c'],
+            ['a', 'b'],
+            ['c', 'b'],
+            ['c', 'd'],
+            ['b', 'd'],
+            ['e', 'd'],
+            ['g', 'f']
+        ]
+        assert shortest_path(edges, 'e', 'c') == 2
+
+    def test_04(self):
+        edges = [
+            ['a', 'c'],
+            ['a', 'b'],
+            ['c', 'b'],
+            ['c', 'd'],
+            ['b', 'd'],
+            ['e', 'd'],
+            ['g', 'f']
+        ]
+        assert shortest_path(edges, 'b', 'g') == -1
+
+    def test_05(self):
+        edges = [
+            ['c', 'n'],
+            ['c', 'e'],
+            ['c', 's'],
+            ['c', 'w'],
+            ['w', 'e'],
+        ]
+        assert shortest_path(edges, 'w', 'e') == 1
+
+    def test_06(self):
+        edges = [
+            ['c', 'n'],
+            ['c', 'e'],
+            ['c', 's'],
+            ['c', 'w'],
+            ['w', 'e'],
+        ]
+        assert shortest_path(edges, 'n', 'e') == 2
+
+    def test_07(self):
+        edges = [
+            ['m', 'n'],
+            ['n', 'o'],
+            ['o', 'p'],
+            ['p', 'q'],
+            ['t', 'o'],
+            ['r', 'q'],
+            ['r', 's']
+        ]
+        assert shortest_path(edges, 'm', 's') == 6
+
+
 if __name__ == '__main__':
-
-    edges = [
-        ['w', 'x'],
-        ['x', 'y'],
-        ['z', 'y'],
-        ['z', 'v'],
-        ['w', 'v']
-    ]
-    assert shortest_path(edges, 'w', 'z') == 2
-
-    edges = [
-        ['w', 'x'],
-        ['x', 'y'],
-        ['z', 'y'],
-        ['z', 'v'],
-        ['w', 'v']
-    ]
-    assert shortest_path(edges, 'y', 'x') == 1
-
-    edges = [
-        ['a', 'c'],
-        ['a', 'b'],
-        ['c', 'b'],
-        ['c', 'd'],
-        ['b', 'd'],
-        ['e', 'd'],
-        ['g', 'f']
-    ]
-    assert shortest_path(edges, 'a', 'e') == 3
-
-    edges = [
-        ['a', 'c'],
-        ['a', 'b'],
-        ['c', 'b'],
-        ['c', 'd'],
-        ['b', 'd'],
-        ['e', 'd'],
-        ['g', 'f']
-    ]
-    assert shortest_path(edges, 'e', 'c') == 2
-
-    edges = [
-        ['a', 'c'],
-        ['a', 'b'],
-        ['c', 'b'],
-        ['c', 'd'],
-        ['b', 'd'],
-        ['e', 'd'],
-        ['g', 'f']
-    ]
-    assert shortest_path(edges, 'b', 'g') == -1
-
-    edges = [
-        ['c', 'n'],
-        ['c', 'e'],
-        ['c', 's'],
-        ['c', 'w'],
-        ['w', 'e'],
-    ]
-    assert shortest_path(edges, 'w', 'e') == 1
-
-    edges = [
-        ['c', 'n'],
-        ['c', 'e'],
-        ['c', 's'],
-        ['c', 'w'],
-        ['w', 'e'],
-    ]
-    assert shortest_path(edges, 'n', 'e') == 2
-
-    edges = [
-        ['m', 'n'],
-        ['n', 'o'],
-        ['o', 'p'],
-        ['p', 'q'],
-        ['t', 'o'],
-        ['r', 'q'],
-        ['r', 's']
-    ]
-    assert shortest_path(edges, 'm', 's') == 6
-
-    print('OK')
+    unittest.main()
