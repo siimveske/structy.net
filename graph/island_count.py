61276d711651e5c45421c93828962afe6b9d5988
Siim Veske <siim.veske@gmail.com>
2022-01-23 13:17:58 +0200

    move island count tests into testcase class

--- graph/island_count.py
+++ graph/island_count.py
@@ -4,6 +4,9 @@ https://www.youtube.com/watch?v=tWVWeAqZ0WU&t=5976s
 '''
 
 
+import unittest
+
+
 def explore(grid, row, col, visited):
     rowInbound = 0 <= row < len(grid)
     colInboud = 0 <= col < len(grid[0])
@@ -17,10 +20,10 @@ def explore(grid, row, col, visited):
 
     visited.add((row, col))
 
-    explore(grid, row-1, col, visited)
-    explore(grid, row+1, col, visited)
-    explore(grid, row, col-1, visited)
-    explore(grid, row, col+1, visited)
+    explore(grid, row - 1, col, visited)
+    explore(grid, row + 1, col, visited)
+    explore(grid, row, col - 1, visited)
+    explore(grid, row, col + 1, visited)
 
     return True
 
@@ -36,38 +39,44 @@ def island_count(grid):
     return island_count
 
 
+class Test(unittest.TestCase):
+    def test_00(self):
+        grid = [
+            ['W', 'L', 'W', 'W', 'W'],
+            ['W', 'L', 'W', 'W', 'W'],
+            ['W', 'W', 'W', 'L', 'W'],
+            ['W', 'W', 'L', 'L', 'W'],
+            ['L', 'W', 'W', 'L', 'L'],
+            ['L', 'L', 'W', 'W', 'W'],
+        ]
+        assert island_count(grid) == 3
+
+    def test_01(self):
+        grid = [
+            ['L', 'W', 'W', 'L', 'W'],
+            ['L', 'W', 'W', 'L', 'L'],
+            ['W', 'L', 'W', 'L', 'W'],
+            ['W', 'W', 'W', 'W', 'W'],
+            ['W', 'W', 'L', 'L', 'L'],
+        ]
+        assert island_count(grid) == 4
+
+    def test_02(self):
+        grid = [
+            ['L', 'L', 'L'],
+            ['L', 'L', 'L'],
+            ['L', 'L', 'L'],
+        ]
+        assert island_count(grid) == 1
+
+    def test_03(self):
+        grid = [
+            ['W', 'W'],
+            ['W', 'W'],
+            ['W', 'W'],
+        ]
+        assert island_count(grid) == 0
+
+
 if __name__ == '__main__':
-    grid = [
-        ['W', 'L', 'W', 'W', 'W'],
-        ['W', 'L', 'W', 'W', 'W'],
-        ['W', 'W', 'W', 'L', 'W'],
-        ['W', 'W', 'L', 'L', 'W'],
-        ['L', 'W', 'W', 'L', 'L'],
-        ['L', 'L', 'W', 'W', 'W'],
-    ]
-    assert island_count(grid) == 3
-
-    grid = [
-        ['L', 'W', 'W', 'L', 'W'],
-        ['L', 'W', 'W', 'L', 'L'],
-        ['W', 'L', 'W', 'L', 'W'],
-        ['W', 'W', 'W', 'W', 'W'],
-        ['W', 'W', 'L', 'L', 'L'],
-    ]
-    assert island_count(grid) == 4
-
-    grid = [
-        ['L', 'L', 'L'],
-        ['L', 'L', 'L'],
-        ['L', 'L', 'L'],
-    ]
-    assert island_count(grid) == 1
-
-    grid = [
-        ['W', 'W'],
-        ['W', 'W'],
-        ['W', 'W'],
-    ]
-    assert island_count(grid) == 0
-
-    print('OK')
+    unittest.main()
