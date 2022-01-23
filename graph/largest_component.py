019fcf2904176b8a961ebe90a62a670c9defd86e
Siim Veske <siim.veske@gmail.com>
2022-01-23 12:45:44 +0200

    solve largest component

--- graph/largest_component.py
+++ graph/largest_component.py
@@ -1,21 +1,12 @@
-from collections import defaultdict, deque
+import unittest
 
 
-graph = {
-    0: [8, 1, 5],
-    1: [0],
-    5: [0, 8],
-    8: [0, 5],
-    2: [3, 4],
-    3: [2, 4],
-    4: [3, 2]
-}
+def largest_component(graph):
+    """Find the size of the largest connected
+    component (island of nodes)in the graph"""
 
-
-def dfs(graph):
-
+    result = 0
     visited = set()
-    largest_component = 0
 
     for start in graph:
         if start in visited:
@@ -31,37 +22,139 @@ def dfs(graph):
             component.add(node)
             stack += graph[node]
 
-        if len(component) > largest_component:
-            largest_component = len(component)
+        if len(component) > result:
+            result = len(component)
 
-    return largest_component
+    return result
 
 
-def dfs_recursive(graph):
+def largest_component_rec(graph):
+    """Find the size of the largest connected
+    component (island of nodes)in the graph"""
 
+    result = 0
     visited = set()
-    largest_component = 0
 
     for node in graph:
         if node in visited:
             continue
         count = explore(graph, node, visited)
-        if count > largest_component:
-            largest_component = count
+        if count > result:
+            result = count
 
-    return largest_component
+    return result
 
 
-def explore(graph, src, visited, count=0):
+def explore(graph, src, visited):
+
+    size = 1
     visited.add(src)
-    count += 1
+
     for node in graph[src]:
         if node in visited:
             continue
-        count = explore(graph, node, visited, count)
-    return count
+        size += explore(graph, node, visited)
+
+    return size
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert largest_component({
+            0: [8, 1, 5],
+            1: [0],
+            5: [0, 8],
+            8: [0, 5],
+            2: [3, 4],
+            3: [2, 4],
+            4: [3, 2]
+        }) == 4
+
+    def test_01(self):
+        assert largest_component({
+            1: [2],
+            2: [1, 8],
+            6: [7],
+            9: [8],
+            7: [6, 8],
+            8: [9, 7, 2]
+        }) == 6
+
+    def test_02(self):
+        assert largest_component({
+            3: [],
+            4: [6],
+            6: [4, 5, 7, 8],
+            8: [6],
+            7: [6],
+            5: [6],
+            1: [2],
+            2: [1]
+        }) == 5
+
+    def test_03(self):
+        assert largest_component({}) == 0
+
+    def test_04(self):
+        assert largest_component({
+            0: [4, 7],
+            1: [],
+            2: [],
+            3: [6],
+            4: [0],
+            6: [3],
+            7: [0],
+            8: []
+        }) == 3
+
+    def test_05(self):
+        assert largest_component_rec({
+            0: [8, 1, 5],
+            1: [0],
+            5: [0, 8],
+            8: [0, 5],
+            2: [3, 4],
+            3: [2, 4],
+            4: [3, 2]
+        }) == 4
+
+    def test_06(self):
+        assert largest_component_rec({
+            1: [2],
+            2: [1, 8],
+            6: [7],
+            9: [8],
+            7: [6, 8],
+            8: [9, 7, 2]
+        }) == 6
+
+    def test_07(self):
+        assert largest_component_rec({
+            3: [],
+            4: [6],
+            6: [4, 5, 7, 8],
+            8: [6],
+            7: [6],
+            5: [6],
+            1: [2],
+            2: [1]
+        }) == 5
+
+    def test_08(self):
+        assert largest_component_rec({}) == 0
+
+    def test_09(self):
+        assert largest_component_rec({
+            0: [4, 7],
+            1: [],
+            2: [],
+            3: [6],
+            4: [0],
+            6: [3],
+            7: [0],
+            8: []
+        }) == 3
 
 
 if __name__ == '__main__':
-    assert dfs(graph) == 4
-    assert dfs_recursive(graph) == 4
+    unittest.main()
