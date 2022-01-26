375afc6a065c3ba11033b70bce1b6ca2ab462b23
Siim Veske <siim.veske@gmail.com>
2022-01-26 18:18:42 +0200

    solve intersection

--- 2. Array and Strings/013_intersection.py
+++ 2. Array and Strings/013_intersection.py
@@ -1,19 +1,49 @@
 """
-intersection
+--- Intersection ---
 Write a function, intersection, that takes in two lists, a,b, as arguments. The function should return a new list containing elements that are in both of the two lists.
 
 You may assume that each input list does not contain duplicate elements.
-
-test_00:
-intersection([4,2,1,6], [3,6,9,2,10]) # -> [2,6]
-test_01:
-intersection([2,4,6], [4,2]) # -> [2,4]
-test_02:
-intersection([4,2,1], [1,2,4,6]) # -> [1,2,4]
-test_03:
-intersection([0,1,2], [10,11]) # -> []
-test_04:
-a = [ i for i in range(0, 50000) ]
-b = [ i for i in range(0, 50000) ]
-intersection(a, b) # -> [0,1,2,3,..., 49999]
 """
+import unittest
+
+
+def intersection(a, b):
+
+    result = []
+
+    if len(a) > len(b):
+        memo = set(a)
+        for number in b:
+            if number in memo:
+                result.append(number)
+    else:
+        memo = set(b)
+        for number in a:
+            if number in memo:
+                result.append(number)
+
+    result.sort()
+    return result
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]) == [2, 6]
+
+    def test_01(self):
+        assert intersection([2, 4, 6], [4, 2]) == [2, 4]
+
+    def test_02(self):
+        assert intersection([4, 2, 1], [1, 2, 4, 6]) == [1, 2, 4]
+
+    def test_03(self):
+        assert intersection([0, 1, 2], [10, 11]) == []
+
+    def test_04(self):
+        a = [i for i in range(0, 50000)]
+        b = [i for i in range(0, 50000)]
+        assert intersection(a, b) == [i for i in range(0, 50000)]
+
+
+if __name__ == "__main__":
+    unittest.main()
