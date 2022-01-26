ae0c4cb091ef91808b49d52cffbc0ca1d7052c8a
Siim Veske <siim.veske@gmail.com>
2022-01-26 17:44:25 +0200

    doc++

--- 2. Array and Strings/011_pair_product.py
+++ 2. Array and Strings/011_pair_product.py
@@ -1,5 +1,5 @@
 """
-pair product
+--- Pair product ---
 Write a function, pair_product, that takes in a list and a target product as arguments. The function should return a tuple containing a pair of indices whose elements multiply to the given target. The indices returned must be unique.
 
 Be sure to return the indices, not the elements themselves.
