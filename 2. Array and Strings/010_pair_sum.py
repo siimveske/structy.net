215e4092d8f5fe64bbbf295e6c8eb74cb4e3e76f
Siim Veske <siim.veske@gmail.com>
2022-01-26 17:14:29 +0200

    solve pair_sum

--- 2. Array and Strings/010_pair_sum.py
+++ 2. Array and Strings/010_pair_sum.py
@@ -1,21 +1,40 @@
 """
-pair sum
+--- Pair sum ---
 Write a function, pair_sum, that takes in a list and a target sum as arguments. The function should return a tuple containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
 
 Be sure to return the indices, not the elements themselves.
 
 There is guaranteed to be one such pair that sums to the target.
-
-test_00:
-pair_sum([3, 2, 5, 4, 1], 8) # -> (0, 2)
-test_01:
-pair_sum([4, 7, 9, 2, 5, 1], 5) # -> (0, 5)
-test_02:
-pair_sum([4, 7, 9, 2, 5, 1], 3) # -> (3, 5)
-test_03:
-pair_sum([1, 6, 7, 2], 13) # -> (1, 2)
-test_04:
-pair_sum([9, 9], 18) # -> (0, 1)
-test_05:
-pair_sum([6, 4, 2, 8 ], 12) # -> (1, 3)
 """
+import unittest
+
+
+def pair_sum(numbers, target_sum):
+    for i in range(len(numbers) - 1):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target_sum:
+                return (i, j)
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert pair_sum([3, 2, 5, 4, 1], 8) == (0, 2)
+
+    def test_01(self):
+        assert pair_sum([4, 7, 9, 2, 5, 1], 5) == (0, 5)
+
+    def test_02(self):
+        assert pair_sum([4, 7, 9, 2, 5, 1], 3) == (3, 5)
+
+    def test_03(self):
+        assert pair_sum([1, 6, 7, 2], 13) == (1, 2)
+
+    def test_04(self):
+        assert pair_sum([9, 9], 18) == (0, 1)
+
+    def test_05(self):
+        assert pair_sum([6, 4, 2, 8], 12) == (1, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
