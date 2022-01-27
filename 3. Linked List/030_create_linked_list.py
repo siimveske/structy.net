34c2bf2a5a0498f7a5b1c17081d23fa0f0ad4fe3
Siim Veske <siim.veske@gmail.com>
2022-01-27 23:08:02 +0200

    add create_linked_list

--- 3. Linked List/030_create_linked_list.py
+++ 3. Linked List/030_create_linked_list.py
@@ -1,17 +1,41 @@
 """
-create linked list
+--- Create linked list ---
 Write a function, create_linked_list, that takes in a list of values as an argument. The function should create a linked list containing each item of the list as the values of the nodes. The function should return the head of the linked list.
-
-test_00:
-create_linked_list(["h", "e", "y"])
-# h -> e -> y
-test_01:
-create_linked_list([1, 7, 1, 8])
-# 1 -> 7 -> 1 -> 8
-test_02:
-create_linked_list(["a"])
-# a
-test_03:
-create_linked_list([])
-# null
 """
+import unittest
+from node import Node
+from _019_linked_list_values import str_list
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    root = Node(None)
+    current = root
+    for value in values:
+        current.next = Node(value)
+        current = current.next
+
+    return root.next
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        result = create_linked_list(["h", "e", "y"])
+        assert str_list(result) == "h -> e -> y"
+
+    def test_01(self):
+        result = create_linked_list([1, 7, 1, 8])
+        assert str_list(result) == "1 -> 7 -> 1 -> 8"
+
+    def test_02(self):
+        result = create_linked_list(["a"])
+        assert str_list(result) == "a"
+
+    def test_03(self):
+        assert create_linked_list([]) == None
+
+
+if __name__ == "__main__":
+    unittest.main()
