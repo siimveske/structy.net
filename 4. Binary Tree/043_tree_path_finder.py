a8c11f67cad2bf3dcb3aff29dccc720eda9e2882
Siim Veske <siim.veske@gmail.com>
2022-01-29 13:28:24 +0200

    implement iterative path_finder

--- 4. Binary Tree/043_tree_path_finder.py
+++ 4. Binary Tree/043_tree_path_finder.py
@@ -7,23 +7,27 @@ You may assume that the tree contains unique values.
 import unittest
 from node import Node
 
-# def path_finder(root, target):
-#     if not root:
-#         return None
-
-#     path = []
-#     stack = [root]
-#     while stack:
-#         node = stack.pop()
-#         if node.val == target:
-#             print()
-#         if node.left:
-#             stack.append(node.left)
-#         if node.right:
-#             stack.append(node.right)
-
 
 def path_finder(root, target):
+    if not root:
+        return None
+
+    stack = [(root, [root.val])]
+    while stack:
+        node, path = stack.pop()
+        if node.val == target:
+            return path
+        if node.left:
+            newpath = path + [node.left.val]
+            stack.append((node.left, newpath))
+        if node.right:
+            newpath = path + [node.right.val]
+            stack.append((node.right, newpath))
+
+    return None
+
+
+def path_finder_rec(root, target):
     result = _path_finder(root, target)
     if result:
         result = result[::-1]
@@ -161,6 +165,7 @@ class Test(unittest.TestCase):
         assert path_finder(None, "x") == None
 
     def test_06(self):
+
         root = Node(0)
         curr = root
         for i in range(1, 19500):
@@ -181,7 +186,140 @@ class Test(unittest.TestCase):
         #                \
         #                19499
 
-        result = path_finder(root, 16281)
+        assert path_finder(root, 16281) == [i for i in range(16282)]
+
+    def test_07(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        assert path_finder_rec(a, 'e') == ['a', 'b', 'e']
+
+    def test_08(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        assert path_finder_rec(a, 'p') == None
+
+    def test_09(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        g = Node("g")
+        h = Node("h")
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        f.right = h
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+        #    /       \
+        #   g         h
+
+        assert path_finder_rec(a, "c") == ['a', 'c']
+
+    def test_10(self):
+        a = Node("a")
+        b = Node("b")
+        c = Node("c")
+        d = Node("d")
+        e = Node("e")
+        f = Node("f")
+        g = Node("g")
+        h = Node("h")
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        f.right = h
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+        #    /       \
+        #   g         h
+
+        assert path_finder_rec(a, "h") == ['a', 'c', 'f', 'h']
+
+    def test_11(self):
+        x = Node("x")
+        assert path_finder_rec(x, "x") == ['x']
+
+    def test_12(self):
+        assert path_finder_rec(None, "x") == None
+
+    def test_13(self):
+
+        self.skipTest("RecursionError")
+
+        root = Node(0)
+        curr = root
+        for i in range(1, 19500):
+            curr.right = Node(i)
+            curr = curr.right
+
+        #      0
+        #       \
+        #        1
+        #         \
+        #          2
+        #           \
+        #            3
+        #             .
+        #              .
+        #               .
+        #              19498
+        #                \
+        #                19499
+
+        assert path_finder_rec(root, 16281) == [i for i in range(16282)]
 
 
 if __name__ == "__main__":
