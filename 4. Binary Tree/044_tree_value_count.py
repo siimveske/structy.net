e56fdcc9ad962cbae4b1d1736db6fb2686b0f816
Siim Veske <siim.veske@gmail.com>
2022-01-29 16:23:17 +0200

    implement iterative tree_value_count

--- 4. Binary Tree/044_tree_value_count.py
+++ 4. Binary Tree/044_tree_value_count.py
@@ -7,7 +7,7 @@ import unittest
 from node import Node
 
 
-def tree_value_count(root: Node, target: int):
+def tree_value_count_rec(root: Node, target: int):
     if not root:
         return 0
 
@@ -15,8 +15,26 @@ def tree_value_count(root: Node, target: int):
     if root.val == target:
         result += 1
 
-    result += tree_value_count(root.left, target)
-    result += tree_value_count(root.right, target)
+    result += tree_value_count_rec(root.left, target)
+    result += tree_value_count_rec(root.right, target)
+
+    return result
+
+
+def tree_value_count(root: Node, target: int):
+    if not root:
+        return 0
+
+    result = 0
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node.val == target:
+            result += 1
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
 
     return result
 
@@ -124,6 +142,109 @@ class Test(unittest.TestCase):
 
     def test_04(self):
         assert tree_value_count(None, 42) == 0
+
+    def test_05(self):
+        a = Node(12)
+        b = Node(6)
+        c = Node(6)
+        d = Node(4)
+        e = Node(6)
+        f = Node(12)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      12
+        #    /   \
+        #   6     6
+        #  / \     \
+        # 4   6     12
+
+        assert tree_value_count_rec(a, 6) == 3
+
+    def test_06(self):
+        a = Node(12)
+        b = Node(6)
+        c = Node(6)
+        d = Node(4)
+        e = Node(6)
+        f = Node(12)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      12
+        #    /   \
+        #   6     6
+        #  / \     \
+        # 4  6     12
+
+        assert tree_value_count_rec(a, 12) == 2
+
+    def test_07(self):
+        a = Node(7)
+        b = Node(5)
+        c = Node(1)
+        d = Node(1)
+        e = Node(8)
+        f = Node(7)
+        g = Node(1)
+        h = Node(1)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        f.right = h
+
+        #      7
+        #    /   \
+        #   5     1
+        #  / \     \
+        # 1   8     7
+        #    /       \
+        #   1         1
+
+        assert tree_value_count_rec(a, 1) == 4
+
+    def test_08(self):
+        a = Node(7)
+        b = Node(5)
+        c = Node(1)
+        d = Node(1)
+        e = Node(8)
+        f = Node(7)
+        g = Node(1)
+        h = Node(1)
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        f.right = h
+
+        #      7
+        #    /   \
+        #   5     1
+        #  / \     \
+        # 1   8     7
+        #    /       \
+        #   1         1
+
+        assert tree_value_count_rec(a, 9) == 0
+
+    def test_09(self):
+        assert tree_value_count_rec(None, 42) == 0
 
 
 if __name__ == "__main__":
