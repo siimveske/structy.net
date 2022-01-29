cbff59c89c103dd34a2229e66a7ead337decbe00
Siim Veske <siim.veske@gmail.com>
2022-01-29 16:54:36 +0200

    implement recursive how_high

--- 4. Binary Tree/045_how_high.py
+++ 4. Binary Tree/045_how_high.py
@@ -29,6 +29,21 @@ def how_high(node):
     return tree_depth
 
 
+def how_high_rec(node):
+    if not node:
+        return -1
+
+    tree_depth = 0
+    if node.left:
+        left = 1 + how_high_rec(node.left)
+        tree_depth = max(tree_depth, left)
+    if node.right:
+        right = 1 + how_high_rec(node.right)
+        tree_depth = max(tree_depth, right)
+
+    return tree_depth
+
+
 class Test(unittest.TestCase):
     def test_00(self):
         a = Node('a')
@@ -50,7 +65,7 @@ class Test(unittest.TestCase):
         #  / \     \
         # d   e     f
 
-        assert how_high(a) == 2
+        assert how_high_rec(a) == 2
 
     def test_01(self):
         a = Node('a')
@@ -76,7 +91,7 @@ class Test(unittest.TestCase):
         #    /
         #   g
 
-        assert how_high(a) == 3
+        assert how_high_rec(a) == 3
 
     def test_02(self):
         a = Node('a')
@@ -88,14 +103,81 @@ class Test(unittest.TestCase):
         #       \
         #        c
 
-        assert how_high(a) == 1
+        assert how_high_rec(a) == 1
 
     def test_03(self):
         a = Node('a')
-        assert how_high(a) == 0
+        assert how_high_rec(a) == 0
 
     def test_04(self):
-        assert how_high(None) == -1
+        assert how_high_rec(None) == -1
+
+    def test_05(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        assert how_high_rec(a) == 2
+
+    def test_06(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+        g = Node('g')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+        #    /
+        #   g
+
+        assert how_high_rec(a) == 3
+
+    def test_07(self):
+        a = Node('a')
+        c = Node('c')
+
+        a.right = c
+
+        #      a
+        #       \
+        #        c
+
+        assert how_high_rec(a) == 1
+
+    def test_08(self):
+        a = Node('a')
+        assert how_high_rec(a) == 0
+
+    def test_09(self):
+        assert how_high_rec(None) == -1
 
 
 if __name__ == "__main__":
