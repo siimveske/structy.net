3d49b1c0eb056cdbe56530a463e833df6bb88246
Siim Veske <siim.veske@gmail.com>
2022-01-31 19:41:20 +0200

    solve all_tree_paths

--- 4. Binary Tree/047_all_tree_paths.py
+++ 4. Binary Tree/047_all_tree_paths.py
@@ -1,108 +1,132 @@
 """
-all tree paths
+--- All tree paths ---
 Write a function, all_tree_paths, that takes in the root of a binary tree. The function should return a 2-Dimensional list where each subarray represents a root-to-leaf path in the tree.
 
 The order within an individual path must start at the root and end at the leaf, but the relative order among paths in the outer list does not matter.
 
 You may assume that the input tree is non-empty.
-
-test_00:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
-
-#      a
-#    /   \
-#   b     c
-#  / \     \
-# d   e     f
-
-all_tree_paths(a) # ->
-# [
-#   [ 'a', 'b', 'd' ],
-#   [ 'a', 'b', 'e' ],
-#   [ 'a', 'c', 'f' ]
-# ]
-test_01:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
-g = Node('g')
-h = Node('h')
-i = Node('i')
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
-e.left = g
-e.right = h
-f.left = i
-
-#         a
-#      /    \
-#     b      c
-#   /  \      \
-#  d    e      f
-#      / \    /
-#     g  h   i
-
-all_tree_paths(a) # ->
-# [
-#   [ 'a', 'b', 'd' ],
-#   [ 'a', 'b', 'e', 'g' ],
-#   [ 'a', 'b', 'e', 'h' ],
-#   [ 'a', 'c', 'f', 'i' ]
-# ]
-test_02:
-q = Node('q')
-r = Node('r')
-s = Node('s')
-t = Node('t')
-u = Node('u')
-v = Node('v')
-
-q.left = r
-q.right = s
-r.right = t
-t.left = u
-u.right = v
-
-#      q
-#    /   \
-#   r     s
-#    \
-#     t
-#    /
-#   u
-#  /
-# v
-
-all_tree_paths(q) # ->
-# [
-#   [ 'q', 'r', 't', 'u', 'v' ],
-#   [ 'q', 's' ]
-# ]
-test_03:
-z = Node('z')
-
-#      z
-
-all_tree_paths(z) # ->
-# [
-#   ['z']
-# ]
 """
+
+import unittest
+from node import Node
+
+
+def all_tree_paths(root):
+    stack = []
+    stack.append((root, [root.val]))
+    results = []
+
+    while stack:
+        node, path = stack.pop()
+        if node.left:
+            stack.append((node.left, [*path, node.left.val]))
+        if node.right:
+            stack.append((node.right, [*path, node.right.val]))
+        if not node.left and not node.right:
+            results.append(path)
+
+    return results
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        all_tree_paths(a) == [
+            ['a', 'b', 'd'],
+            ['a', 'b', 'e'],
+            ['a', 'c', 'f']
+        ]
+
+    def test_01(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+        g = Node('g')
+        h = Node('h')
+        i = Node('i')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        e.right = h
+        f.left = i
+
+        #         a
+        #      /    \
+        #     b      c
+        #   /  \      \
+        #  d    e      f
+        #      / \    /
+        #     g  h   i
+
+        all_tree_paths(a) == [
+            ['a', 'b', 'd'],
+            ['a', 'b', 'e', 'g'],
+            ['a', 'b', 'e', 'h'],
+            ['a', 'c', 'f', 'i']
+        ]
+
+    def test_02(self):
+        q = Node('q')
+        r = Node('r')
+        s = Node('s')
+        t = Node('t')
+        u = Node('u')
+        v = Node('v')
+
+        q.left = r
+        q.right = s
+        r.right = t
+        t.left = u
+        u.right = v
+
+        #      q
+        #    /   \
+        #   r     s
+        #    \
+        #     t
+        #    /
+        #   u
+        #  /
+        # v
+
+        all_tree_paths(q) == [
+            ['q', 'r', 't', 'u', 'v'],
+            ['q', 's']
+        ]
+
+    def test_03(self):
+        z = Node('z')
+        # z
+
+        all_tree_paths(z) == [
+            ['z']
+        ]
+
+
+if __name__ == "__main__":
+    unittest.main()
