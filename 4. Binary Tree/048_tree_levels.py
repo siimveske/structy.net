a6666549c593e841eca5cee4858b8eac2b83a8e3
Siim Veske <siim.veske@gmail.com>
2022-01-31 23:29:46 +0200

    solve tree_levels

--- 4. Binary Tree/048_tree_levels.py
+++ 4. Binary Tree/048_tree_levels.py
@@ -1,100 +1,133 @@
 """
-tree levels
+--- Tree levels ---
 Write a function, tree_levels, that takes in the root of a binary tree. The function should return a 2-Dimensional list where each sublist represents a level of the tree.
-
-test_00:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
-
-#      a
-#    /   \
-#   b     c
-#  / \     \
-# d   e     f
-
-tree_levels(a) # ->
-# [
-#   ['a'],
-#   ['b', 'c'],
-#   ['d', 'e', 'f']
-# ]
-test_01:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
-g = Node('g')
-h = Node('h')
-i = Node('i')
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
-e.left = g
-e.right = h
-f.left = i
-
-#         a
-#      /    \
-#     b      c
-#   /  \      \
-#  d    e      f
-#      / \    /
-#     g  h   i
-
-tree_levels(a) # ->
-# [
-#   ['a'],
-#   ['b', 'c'],
-#   ['d', 'e', 'f'],
-#   ['g', 'h', 'i']
-# ]
-test_02:
-q = Node('q')
-r = Node('r')
-s = Node('s')
-t = Node('t')
-u = Node('u')
-v = Node('v')
-
-q.left = r
-q.right = s
-r.right = t
-t.left = u
-u.right = v
-
-#      q
-#    /   \
-#   r     s
-#    \
-#     t
-#    /
-#   u
-#  /
-# v
-
-tree_levels(q) # ->
-# [
-#   ['q'],
-#   ['r', 's'],
-#   ['t'],
-#   ['u'],
-#   ['v']
-# ]
-test_03:
-tree_levels(None) # -> []
 """
+
+import unittest
+from node import Node
+from collections import deque
+from collections import defaultdict
+
+
+def tree_levels(root):
+    if not root:
+        return []
+
+    queue = deque([(root, 0)])
+    levels = []
+    result = defaultdict(list)
+
+    while queue:
+        node, level = queue.popleft()
+        result[level].append(node.val)
+        if node.left:
+            queue.append((node.left, level + 1))
+        if node.right:
+            queue.append((node.right, level + 1))
+
+    for key in result:
+        levels.append(result[key])
+
+    return levels
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+
+        #      a
+        #    /   \
+        #   b     c
+        #  / \     \
+        # d   e     f
+
+        assert tree_levels(a) == [
+            ['a'],
+            ['b', 'c'],
+            ['d', 'e', 'f']
+        ]
+
+    def test_01(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+        g = Node('g')
+        h = Node('h')
+        i = Node('i')
+
+        a.left = b
+        a.right = c
+        b.left = d
+        b.right = e
+        c.right = f
+        e.left = g
+        e.right = h
+        f.left = i
+
+        #         a
+        #      /    \
+        #     b      c
+        #   /  \      \
+        #  d    e      f
+        #      / \    /
+        #     g  h   i
+
+        assert tree_levels(a) == [
+            ['a'],
+            ['b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i']
+        ]
+
+    def test_02(self):
+        q = Node('q')
+        r = Node('r')
+        s = Node('s')
+        t = Node('t')
+        u = Node('u')
+        v = Node('v')
+
+        q.left = r
+        q.right = s
+        r.right = t
+        t.left = u
+        u.right = v
+
+        #      q
+        #    /   \
+        #   r     s
+        #    \
+        #     t
+        #    /
+        #   u
+        #  /
+        # v
+
+        assert tree_levels(q) == [
+            ['q'],
+            ['r', 's'],
+            ['t'],
+            ['u'],
+            ['v']
+        ]
+
+    def test_03(self):
+        assert tree_levels(None) == []
+
+
+if __name__ == "__main__":
+    unittest.main()
