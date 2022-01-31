2fb85ecdfa3d407580753a1b093e65598cbebe17
Siim Veske <siim.veske@gmail.com>
2022-01-31 20:58:07 +0200

    add recursive version of all_tree_paths

--- 4. Binary Tree/047_all_tree_paths.py
+++ 4. Binary Tree/047_all_tree_paths.py
@@ -11,21 +11,40 @@ import unittest
 from node import Node
 
 
+# def all_tree_paths(root):
+#     stack = []
+#     stack.append((root, [root.val]))
+#     results = []
+
+#     while stack:
+#         node, path = stack.pop()
+#         if node.left:
+#             stack.append((node.left, [*path, node.left.val]))
+#         if node.right:
+#             stack.append((node.right, [*path, node.right.val]))
+#         if not node.left and not node.right:
+#             results.append(path)
+
+#     return results
+
+
 def all_tree_paths(root):
-    stack = []
-    stack.append((root, [root.val]))
-    results = []
+    if not root:
+        return []
+    if not root.left and not root.right:
+        return [[root.val]]
 
-    while stack:
-        node, path = stack.pop()
-        if node.left:
-            stack.append((node.left, [*path, node.left.val]))
-        if node.right:
-            stack.append((node.right, [*path, node.right.val]))
-        if not node.left and not node.right:
-            results.append(path)
+    paths = []
 
-    return results
+    left = all_tree_paths(root.left)
+    for path in left:
+        paths.append([root.val, *path])
+
+    right = all_tree_paths(root.right)
+    for path in right:
+        paths.append([root.val, *path])
+
+    return paths
 
 
 class Test(unittest.TestCase):
