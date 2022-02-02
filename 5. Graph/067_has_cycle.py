4eefc12f772e6070fb565c7a412b2b1618d62381
Siim Veske <siim.veske@gmail.com>
2022-02-02 13:10:33 +0200

    add recursive has_cycle

--- 5. Graph/067_has_cycle.py
+++ 5. Graph/067_has_cycle.py
@@ -7,29 +7,54 @@ Write a function, has_cycle, that takes in an object representing the adjacency 
 import unittest
 
 
+# def has_cycle(graph):
+#     visited = set()
+#     for node in graph:
+#         stack = [node]
+#         visiting = set()
+#         while stack:
+#             current = stack.pop()
+
+#             if current in visiting:
+#                 return True
+#             if current in visited:
+#                 continue
+
+#             visiting.add(current)
+
+#             if not graph[current]:
+#                 visited = visited.union(visiting)
+#                 visiting = set()
+
+#             for neighbor in graph[current]:
+#                 stack.append(neighbor)
+
+#         visited = visited.union(visiting)
+#     return False
+
 def has_cycle(graph):
     visited = set()
-    for node in graph:
-        stack = [node]
-        visiting = set()
-        while stack:
-            current = stack.pop()
+    for start_node in graph:
+        if cycle_detect(graph, start_node, set(), visited):
+            return True
+    return False
 
-            if current in visiting:
-                return True
-            if current in visited:
-                continue
 
-            visiting.add(current)
+def cycle_detect(graph, node, visiting, visited):
+    if node in visited:
+        return False
 
-            if not graph[current]:
-                visited = visited.union(visiting)
-                visiting = set()
+    if node in visiting:
+        return True
 
-            for neighbor in graph[current]:
-                stack.append(neighbor)
+    visiting.add(node)
 
-        visited = visited.union(visiting)
+    for neighbor in graph[node]:
+        if cycle_detect(graph, neighbor, visiting, visited):
+            return True
+
+    visiting.remove(node)
+    visited.add(node)
     return False
 
 
