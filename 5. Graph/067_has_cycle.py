e78bdcadf8701f6cbdbfca0459fd6c35038ca52b
Siim Veske <siim.veske@gmail.com>
2022-02-02 11:49:49 +0200

    solve has_cycle

--- 5. Graph/067_has_cycle.py
+++ 5. Graph/067_has_cycle.py
@@ -1,44 +1,78 @@
 """
-has cycle
+--- Has cycle ---
 Write a function, has_cycle, that takes in an object representing the adjacency list of a directed graph. The function should return a boolean indicating whether or not the graph contains a cycle.
-
-test_00:
-has_cycle({
-  "a": ["b"],
-  "b": ["c"],
-  "c": ["a"],
-}) # -> True
-test_01:
-has_cycle({
-  "a": ["b", "c"],
-  "b": ["c"],
-  "c": ["d"],
-  "d": [],
-}) # -> False
-test_02:
-has_cycle({
-  "a": ["b", "c"],
-  "b": [],
-  "c": [],
-  "e": ["f"],
-  "f": ["e"],
-}) # -> True
-test_03:
-has_cycle({
-  "q": ["r", "s"],
-  "r": ["t", "u"],
-  "s": [],
-  "t": [],
-  "u": [],
-  "v": ["w"],
-  "w": [],
-  "x": ["w"],
-}) # -> False
-test_04:
-has_cycle({
-  "a": ["b"],
-  "b": ["c"],
-  "c": ["a"],
-  "g": [],
-}) # -> True
 """
+
+
+import unittest
+
+
+def has_cycle(graph):
+    visited = set()
+    for node in graph:
+        stack = [node]
+        visiting = set()
+        while stack:
+            current = stack.pop()
+            if current in visiting:
+                return True
+
+            visiting.add(current)
+
+            if not graph[current] or current in visited:
+                visited.union(visiting)
+                break
+
+            for neighbor in graph[current]:
+                stack.append(neighbor)
+    return False
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert has_cycle({
+            "a": ["b"],
+            "b": ["c"],
+            "c": ["a"],
+        }) == True
+
+    def test_01(self):
+        assert has_cycle({
+            "a": ["b", "c"],
+            "b": ["c"],
+            "c": ["d"],
+            "d": [],
+        }) == False
+
+    def test_02(self):
+        assert has_cycle({
+            "a": ["b", "c"],
+            "b": [],
+            "c": [],
+            "e": ["f"],
+            "f": ["e"],
+        }) == True
+
+    def test_03(self):
+        assert has_cycle({
+            "q": ["r", "s"],
+            "r": ["t", "u"],
+            "s": [],
+            "t": [],
+            "u": [],
+            "v": ["w"],
+            "w": [],
+            "x": ["w"],
+        }) == False
+
+    def test_04(self):
+        assert has_cycle({
+            "a": ["b"],
+            "b": ["c"],
+            "c": ["a"],
+            "g": [],
+        }) == True
+
+
+if __name__ == "__main__":
+    unittest.main()
