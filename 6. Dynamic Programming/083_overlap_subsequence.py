3edeaa54a5ba3f2c6bbdcd8b91fa0c85c5c1d6f5
Siim Veske <siim.veske@gmail.com>
2022-02-04 22:50:07 +0200

    solve overlap_subsequence

--- 6. Dynamic Programming/083_overlap_subsequence.py
+++ 6. Dynamic Programming/083_overlap_subsequence.py
@@ -1,20 +1,53 @@
 """
-overlap subsequence
-Write a function, overlap_subsequence, that takes in two strings as arguments. The function should return the length of the longest overlapping subsequence.
-
-A subsequence of a string can be created by deleting any characters of the string, while maintaining the relative order of characters.
-
-test_00:
-overlap_subsequence("dogs", "daogt") # -> 3
-test_01:
-overlap_subsequence("xcyats", "criaotsi") # -> 4
-test_02:
-overlap_subsequence("xfeqortsver", "feeeuavoeqr") # -> 5
-test_03:
-overlap_subsequence("kinfolklivemustache", "bespokekinfolksnackwave") # -> 11
-test_04:
-overlap_subsequence(
-  "mumblecorebeardleggingsauthenticunicorn",
-  "succulentspughumblemeditationlocavore"
-) # -> 15
+--- Overlap subsequence ---
 """
+
+
+import unittest
+
+
+def overlap_subsequence(string_1, string_2):
+    return calculate(string_1, string_2, (0, 0), {})
+
+
+def calculate(string_1, string_2, idx, memo):
+    idx1, idx2 = idx
+    if idx in memo:
+        return memo[idx]
+    if idx1 >= len(string_1) or idx2 >= len(string_2):
+        return 0
+
+    result = 0
+    if string_1[idx1] == string_2[idx2]:
+        result += calculate(string_1, string_2, (idx1 + 1, idx2 + 1), memo) + 1
+    else:
+        left = calculate(string_1, string_2, (idx1 + 1, idx2), memo)
+        right = calculate(string_1, string_2, (idx1, idx2 + 1), memo)
+        result += max(left, right)
+
+    memo[idx] = result
+    return memo[idx]
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert overlap_subsequence("dogs", "daogt") == 3
+
+    def test_01(self):
+        assert overlap_subsequence("xcyats", "criaotsi") == 4
+
+    def test_02(self):
+        assert overlap_subsequence("xfeqortsver", "feeeuavoeqr") == 5
+
+    def test_03(self):
+        assert overlap_subsequence("kinfolklivemustache", "bespokekinfolksnackwave") == 11
+
+    def test_04(self):
+        assert overlap_subsequence(
+            "mumblecorebeardleggingsauthenticunicorn",
+            "succulentspughumblemeditationlocavore"
+        ) == 15
+
+
+if __name__ == "__main__":
+    unittest.main()
