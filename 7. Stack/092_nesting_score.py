24b3d302909e31ad014ac61998d2de669f04afa4
Siim Veske <siim.veske@gmail.com>
2022-02-06 20:59:23 +0200

    solve nesting_score

--- 7. Stack/092_nesting_score.py
+++ 7. Stack/092_nesting_score.py
@@ -1,26 +1,54 @@
 """
-nesting score
+--- Nesting score ---
 Write a function, nesting_score, that takes in a string of brackets as an argument. The function should return the score of the string according to the following rules:
+    1. [] is worth 1 point
+    2. XY is worth m + n points where X, Y are substrings of well-formed brackets and m, n are their respective scores
+    3. [S] is worth 2 * k points where S is a substring of well-formed brackets and k is the score of that substring
 
-[] is worth 1 point
-XY is worth m + n points where X, Y are substrings of well-formed brackets and m, n are their respective scores
-[S] is worth 2 * k points where S is a substring of well-formed brackets and k is the score of that substring
 You may assume that the input only contains well-formed square brackets.
-
-test_00:
-nesting_score("[]") # -> 1
-test_01:
-nesting_score("[][][]") # -> 3
-test_02:
-nesting_score("[[]]") # -> 2
-test_03:
-nesting_score("[[][]]") # -> 4
-test_04:
-nesting_score("[[][][]]") # -> 6
-test_05:
-nesting_score("[[][]][]") # -> 5
-test_06:
-nesting_score("[][[][]][[]]") # -> 7
-test_07:
-nesting_score("[[[[[[[][]]]]]]][]") # -> 129
 """
+import unittest
+
+
+def nesting_score(string):
+    stack = [0]
+    for character in string:
+        if character == "[":
+            stack.append(0)
+        else:
+            top = stack.pop()
+            if top:
+                stack[-1] += top * 2
+            else:
+                stack[-1] += 1
+    return stack.pop()
+
+
+class Test(unittest.TestCase):
+    # def test_00(self):
+    #     assert nesting_score("[]") == 1
+
+    # def test_01(self):
+    #     assert nesting_score("[][][]") == 3
+
+    # def test_02(self):
+    #     assert nesting_score("[[]]") == 2
+
+    # def test_03(self):
+    #     assert nesting_score("[[][]]") == 4
+
+    # def test_04(self):
+    #     assert nesting_score("[[][][]]") == 6
+
+    # def test_05(self):
+    #     assert nesting_score("[[][]][]") == 5
+
+    def test_06(self):
+        assert nesting_score("[][[][]][[]]") == 7
+
+    # def test_07(self):
+    #     assert nesting_score("[[[[[[[][]]]]]]][]") == 129
+
+
+if __name__ == "__main__":
+    unittest.main()
