2009d8bb826d66b3db6c44347847ce445962d82e
Siim Veske <siim.veske@gmail.com>
2022-02-07 21:52:30 +0200

    add creative iterative solution for subsets

--- 8. Exhaustive Recursion/095_subsets.py
+++ 8. Exhaustive Recursion/095_subsets.py
@@ -9,18 +9,34 @@ You may assume that the input list contains unique elements.
 import unittest
 
 
+# def subsets(elements):
+#     if len(elements) == 0:
+#         return [[]]
+
+#     first = elements[0]
+#     without_first = subsets(elements[1:])
+
+#     with_first = []
+#     for i in without_first:
+#         with_first.append([first, *i])
+
+#     return without_first + with_first
+
+
 def subsets(elements):
-    if len(elements) == 0:
-        return [[]]
+    """A is an iterable (list, tuple, set, str, etc)
+    returns a set which is the power set of A.
+    url: https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
+    """
+    length = len(elements)
+    powerset = []
 
-    first = elements[0]
-    without_first = subsets(elements[1:])
+    for i in range(2 ** length):
+        selector = f'{i:0{length}b}'
+        subset = [elements[j] for j, bit in enumerate(selector) if bit == '1']
+        powerset.append(subset)
 
-    with_first = []
-    for i in without_first:
-        with_first.append([first, *i])
-
-    return without_first + with_first
+    return powerset
 
 
 class Test(unittest.TestCase):
