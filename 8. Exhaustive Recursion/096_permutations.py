bc89a8aa8329f3514c579d290856076d0863398e
Siim Veske <siim.veske@gmail.com>
2022-02-08 12:14:47 +0200

    add solution to permutations

--- 8. Exhaustive Recursion/096_permutations.py
+++ 8. Exhaustive Recursion/096_permutations.py
@@ -1,46 +1,66 @@
 """
-permutations
+--- Permutations ---
 Write a function, permutations, that takes in a list an argument. The function should return a 2D list where each sublist represents one of the possible permutations of the list.
 
 The sublists may be returned in any order.
 
 You may assume that the input list contains unique items.
-
-test_00:
-permutations(['a', 'b', 'c']) # ->
-# [
-#   [ 'a', 'b', 'c' ],
-#   [ 'b', 'a', 'c' ],
-#   [ 'b', 'c', 'a' ],
-#   [ 'a', 'c', 'b' ],
-#   [ 'c', 'a', 'b' ],
-#   [ 'c', 'b', 'a' ]
-# ]
-test_01:
-permutations(['red', 'blue']) # ->
-# [
-#   [ 'red', 'blue' ],
-#   [ 'blue', 'red' ]
-# ]
-test_02:
-permutations([8, 2, 1, 4]) # ->
-# [
-#   [ 8, 2, 1, 4 ], [ 2, 8, 1, 4 ],
-#   [ 2, 1, 8, 4 ], [ 2, 1, 4, 8 ],
-#   [ 8, 1, 2, 4 ], [ 1, 8, 2, 4 ],
-#   [ 1, 2, 8, 4 ], [ 1, 2, 4, 8 ],
-#   [ 8, 1, 4, 2 ], [ 1, 8, 4, 2 ],
-#   [ 1, 4, 8, 2 ], [ 1, 4, 2, 8 ],
-#   [ 8, 2, 4, 1 ], [ 2, 8, 4, 1 ],
-#   [ 2, 4, 8, 1 ], [ 2, 4, 1, 8 ],
-#   [ 8, 4, 2, 1 ], [ 4, 8, 2, 1 ],
-#   [ 4, 2, 8, 1 ], [ 4, 2, 1, 8 ],
-#   [ 8, 4, 1, 2 ], [ 4, 8, 1, 2 ],
-#   [ 4, 1, 8, 2 ], [ 4, 1, 2, 8 ]
-# ]
-test_03:
-permutations([]) # ->
-# [
-#  [ ]
-# ]
 """
+import unittest
+
+
+def permutations(items):
+    if not items:
+        return [[]]
+
+    first = items[0]
+    remaining = items[1:]
+    perms = permutations(remaining)
+
+    result = []
+    for perm in perms:
+        for i in range(len(perm) + 1):
+            result.append(perm[:i] + [first] + perm[i:])
+
+    return result
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert permutations(['a', 'b', 'c']) == [
+            ['a', 'b', 'c'],
+            ['b', 'a', 'c'],
+            ['b', 'c', 'a'],
+            ['a', 'c', 'b'],
+            ['c', 'a', 'b'],
+            ['c', 'b', 'a']
+        ]
+
+    def test_01(self):
+        assert permutations(['red', 'blue']) == [
+            ['red', 'blue'],
+            ['blue', 'red']
+        ]
+
+    def test_02(self):
+        assert permutations([8, 2, 1, 4]) == [
+            [8, 2, 1, 4], [2, 8, 1, 4],
+            [2, 1, 8, 4], [2, 1, 4, 8],
+            [8, 1, 2, 4], [1, 8, 2, 4],
+            [1, 2, 8, 4], [1, 2, 4, 8],
+            [8, 1, 4, 2], [1, 8, 4, 2],
+            [1, 4, 8, 2], [1, 4, 2, 8],
+            [8, 2, 4, 1], [2, 8, 4, 1],
+            [2, 4, 8, 1], [2, 4, 1, 8],
+            [8, 4, 2, 1], [4, 8, 2, 1],
+            [4, 2, 8, 1], [4, 2, 1, 8],
+            [8, 4, 1, 2], [4, 8, 1, 2],
+            [4, 1, 8, 2], [4, 1, 2, 8]
+        ]
+
+    def test_03(self):
+        assert permutations([]) == [[]]
+
+
+if __name__ == "__main__":
+    unittest.main()
