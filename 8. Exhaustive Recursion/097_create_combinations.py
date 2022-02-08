b35ea5612ac182b58ebf9f18fcf8c33d0ebb7da1
Siim Veske <siim.veske@gmail.com>
2022-02-08 18:42:55 +0200

    iterative solution for create_combinations

--- 8. Exhaustive Recursion/097_create_combinations.py
+++ 8. Exhaustive Recursion/097_create_combinations.py
@@ -1,39 +1,58 @@
 """
-create combinations
+--- Create combinations ---
 Write a function, create_combinations, that takes in a list and a length as arguments. The function should return a 2D list representing all of the combinations of the specifized length.
 
 The items within the combinations and the combinations themselves may be returned in any order.
 
 You may assume that the input list contains unique elements and 1 <= k <= len(items).
-
-test_00:
-create_combinations(["a", "b", "c"], 2); # ->
-# [
-#   [ 'a', 'b' ],
-#   [ 'a', 'c' ],
-#   [ 'b', 'c' ]
-# ]
-test_01:
-create_combinations(["q", "r", "s", "t"], 2); # ->
-# [
-#   [ 'q', 'r' ],
-#   [ 'q', 's' ],
-#   [ 'q', 't' ],
-#   [ 'r', 's' ],
-#   [ 'r', 't' ],
-#   [ 's', 't' ]
-# ]
-test_02:
-create_combinations(['q', 'r', 's', 't'], 3)); # ->
-# [
-#   [ 'q', 'r', 's' ],
-#   [ 'q', 'r', 't' ],
-#   [ 'q', 's', 't' ],
-#   [ 'r', 's', 't' ]
-# ]
-test_03:
-create_combinations([1, 28, 94], 3); # ->
-# [
-#   [ 1, 28, 94 ]
-# ]
 """
+import unittest
+
+
+def create_combinations(items, k):
+    combinations = [[]]
+    for item in items:
+        tmp = []
+        for subset in combinations:
+            tmp.append(subset)
+            tmp.append(subset + [item])
+        combinations = tmp
+
+    result = [i for i in combinations if len(i) == k]
+    return result
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        create_combinations(["a", "b", "c"], 2) == [
+            ['a', 'b'],
+            ['a', 'c'],
+            ['b', 'c']
+        ]
+
+    def test_01(self):
+        create_combinations(["q", "r", "s", "t"], 2) == [
+            ['q', 'r'],
+            ['q', 's'],
+            ['q', 't'],
+            ['r', 's'],
+            ['r', 't'],
+            ['s', 't']
+        ]
+
+    def test_02(self):
+        create_combinations(['q', 'r', 's', 't'], 3) == [
+            ['q', 'r', 's'],
+            ['q', 'r', 't'],
+            ['q', 's', 't'],
+            ['r', 's', 't']
+        ]
+
+    def test_03(self):
+        create_combinations([1, 28, 94], 3) == [
+            [1, 28, 94]
+        ]
+
+
+if __name__ == "__main__":
+    unittest.main()
