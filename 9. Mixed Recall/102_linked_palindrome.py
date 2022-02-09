bf918150c8a3208802ece55e62f3fa00c280dbf3
Siim Veske <siim.veske@gmail.com>
2022-02-09 18:43:19 +0200

    solve linked_palindrome

--- 9. Mixed Recall/102_linked_palindrome.py
+++ 9. Mixed Recall/102_linked_palindrome.py
@@ -1,76 +1,115 @@
 """
-linked palindrome
+--- Linked palindrome ---
 Write a function, linked_palindrome, that takes in the head of a linked list as an argument. The function should return a boolean indicating whether or not the linked list is a palindrome. A palindrome is a sequence that is the same both forwards and backwards.
-
-test_00:
-a = Node(3)
-b = Node(2)
-c = Node(7)
-d = Node(7)
-e = Node(2)
-f = Node(3)
-
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-e.next = f
-
-# 3 -> 2 -> 7 -> 7 -> 2 -> 3
-linked_palindrome(a) # True
-test_01:
-a = Node(3)
-b = Node(2)
-c = Node(4)
-
-a.next = b
-b.next = c
-
-# 3 -> 2 -> 4
-linked_palindrome(a) # False
-test_02:
-a = Node(3)
-b = Node(2)
-c = Node(3)
-
-a.next = b
-b.next = c
-
-# 3 -> 2 -> 3
-linked_palindrome(a) # True
-test_03:
-a = Node(0)
-b = Node(1)
-c = Node(0)
-d = Node(1)
-e = Node(0)
-
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-
-# 0 -> 1 -> 0 -> 1 -> 0
-linked_palindrome(a) # True
-test_04:
-a = Node(0)
-b = Node(1)
-c = Node(0)
-d = Node(1)
-e = Node(1)
-
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-
-# 0 -> 1 -> 0 -> 1 -> 1
-linked_palindrome(a) # False
-test_05:
-a = Node(5)
-
-# 5
-linked_palindrome(a) # True
-test_06:
-linked_palindrome(None) # True
 """
+import unittest
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def linked_palindrome(head: Node):
+    if not head:
+        return True
+
+    stack = [head]
+    current = head
+    while current.next:
+        current = current.next
+        stack.append(current)
+
+    current = head
+    while stack:
+        tail = stack.pop()
+        if current.val != tail.val:
+            return False
+        current = current.next
+
+    return True
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node(3)
+        b = Node(2)
+        c = Node(7)
+        d = Node(7)
+        e = Node(2)
+        f = Node(3)
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+        e.next = f
+
+        # 3 -> 2 -> 7 -> 7 -> 2 -> 3
+        assert linked_palindrome(a) == True
+
+    def test_01(self):
+        a = Node(3)
+        b = Node(2)
+        c = Node(4)
+
+        a.next = b
+        b.next = c
+
+        # 3 -> 2 -> 4
+        assert linked_palindrome(a) == False
+
+    def test_02(self):
+        a = Node(3)
+        b = Node(2)
+        c = Node(3)
+
+        a.next = b
+        b.next = c
+
+        # 3 -> 2 -> 3
+        assert linked_palindrome(a) == True
+
+    def test_03(self):
+        a = Node(0)
+        b = Node(1)
+        c = Node(0)
+        d = Node(1)
+        e = Node(0)
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+
+        # 0 -> 1 -> 0 -> 1 -> 0
+        assert linked_palindrome(a) == True
+
+    def test_04(self):
+        a = Node(0)
+        b = Node(1)
+        c = Node(0)
+        d = Node(1)
+        e = Node(1)
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+
+        # 0 -> 1 -> 0 -> 1 -> 1
+        assert linked_palindrome(a) == False
+
+    def test_05(self):
+        a = Node(5)
+
+        # 5
+        assert linked_palindrome(a) == True
+
+    def test_06(self):
+        assert linked_palindrome(None) == True
+
+
+if __name__ == "__main__":
+    unittest.main()
