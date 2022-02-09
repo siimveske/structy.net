5bc64b0cad19f4b9f84b4278b2a288e0c490b0b0
Siim Veske <siim.veske@gmail.com>
2022-02-09 19:05:43 +0200

    solve middle_value

--- 9. Mixed Recall/103_middle_value.py
+++ 9. Mixed Recall/103_middle_value.py
@@ -1,60 +1,88 @@
 """
-middle value
+--- Middle value ---
 Write a function, middle_value, that takes in the head of a linked list as an argument. The function should return the value of the middle node in the linked list. If the linked list has an even number of nodes, then return the value of the second middle node.
 
 You may assume that the input list is non-empty.
-
-test_00:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-
-# a -> b -> c -> d -> e
-middle_value(a) # c
-test_01:
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
-
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-e.next = f
-
-# a -> b -> c -> d -> e -> f
-middle_value(a) # d
-test_02:
-x = Node('x')
-y = Node('y')
-z = Node('z')
-
-x.next = y
-y.next = z
-
-# x -> y -> z
-middle_value(x) # y
-test_03:
-x = Node('x')
-y = Node('y')
-
-x.next = y
-
-# x -> y
-middle_value(x) # y
-test_04:
-q = Node('q')
-
-# q
-middle_value(q) # q
 """
+import unittest
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def middle_value(head: Node):
+    values = []
+    current = head
+    while current:
+        values.append(current.val)
+        current = current.next
+
+    idx = len(values) // 2
+    return values[idx]
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+
+        # a -> b -> c -> d -> e
+        assert middle_value(a) == 'c'
+
+    def test_01(self):
+        a = Node('a')
+        b = Node('b')
+        c = Node('c')
+        d = Node('d')
+        e = Node('e')
+        f = Node('f')
+
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+        e.next = f
+
+        # a -> b -> c -> d -> e -> f
+        assert middle_value(a) == 'd'
+
+    def test_02(self):
+        x = Node('x')
+        y = Node('y')
+        z = Node('z')
+
+        x.next = y
+        y.next = z
+
+        # x -> y -> z
+        assert middle_value(x) == 'y'
+
+    def test_03(self):
+        x = Node('x')
+        y = Node('y')
+
+        x.next = y
+
+        # x -> y
+        assert middle_value(x) == 'y'
+
+    def test_04(self):
+        q = Node('q')
+
+        # q
+        assert middle_value(q) == 'q'
+
+
+if __name__ == "__main__":
+    unittest.main()
