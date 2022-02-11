a2d020d897619cd87cb3772ee88cce43689b8571
Siim Veske <siim.veske@gmail.com>
2022-02-11 20:10:03 +0200

    solve can_color

--- 9. Mixed Recall/108_can_color.py
+++ 9. Mixed Recall/108_can_color.py
@@ -1,5 +1,5 @@
 """
-can color
+--- Can color ---
 Write a function, can_color, that takes in a dictionary representing the adjacency list of an undirected graph. The function should return a boolean indicating whether or not it is possible to color nodes of the graph using two colors in such a way that adjacent nodes are always different colors.
 
 For example, given this graph:
@@ -17,52 +17,88 @@ For example, given this graph:
 
 It is not possible to color the nodes without making two
 adjacent nodes the same color. So the answer is False.
-test_00:
-can_color({
-  "x": ["y"],
-  "y": ["x","z"],
-  "z": ["y"]
-}) # -> True
-test_01:
-can_color({
-  "q": ["r", "s"],
-  "r": ["q", "s"],
-  "s": ["r", "q"]
-}) # -> False
-test_02:
-can_color({
-  "a": ["b", "c", "d"],
-  "b": ["a"],
-  "c": ["a"],
-  "d": ["a"],
-}) # -> True
-test_03:
-can_color({
-  "a": ["b", "c", "d"],
-  "b": ["a"],
-  "c": ["a", "d"],
-  "d": ["a", "c"],
-}) # -> False
-test_04:
-can_color({
-  "h": ["i", "k"],
-  "i": ["h", "j"],
-  "j": ["i", "k"],
-  "k": ["h", "j"],
-}) # -> True
-test_05:
-can_color({
-  "z": []
-}) # -> True
-
-test_06:
-can_color({
-  "h": ["i", "k"],
-  "i": ["h", "j"],
-  "j": ["i", "k"],
-  "k": ["h", "j"],
-  "q": ["r", "s"],
-  "r": ["q", "s"],
-  "s": ["r", "q"]
-}) # -> False
 """
+import unittest
+
+
+def can_color(graph):
+    colors = {}
+    for node in graph:
+        if node in colors:
+            continue
+        if explore(graph, node, colors, True) == False:
+            return False
+    return True
+
+
+def explore(graph, node, colors, color):
+    if node in colors:
+        return colors[node] == color
+
+    colors[node] = color
+
+    for neighbor in graph[node]:
+        if explore(graph, neighbor, colors, not color) == False:
+            return False
+
+    return True
+
+
+class Test(unittest.TestCase):
+    def test_00(self):
+        assert can_color({
+            "x": ["y"],
+            "y": ["x", "z"],
+            "z": ["y"]
+        }) == True
+
+    def test_01(self):
+        assert can_color({
+            "q": ["r", "s"],
+            "r": ["q", "s"],
+            "s": ["r", "q"]
+        }) == False
+
+    def test_02(self):
+        assert can_color({
+            "a": ["b", "c", "d"],
+            "b": ["a"],
+            "c": ["a"],
+            "d": ["a"],
+        }) == True
+
+    def test_03(self):
+        assert can_color({
+            "a": ["b", "c", "d"],
+            "b": ["a"],
+            "c": ["a", "d"],
+            "d": ["a", "c"],
+        }) == False
+
+    def test_04(self):
+        assert can_color({
+            "h": ["i", "k"],
+            "i": ["h", "j"],
+            "j": ["i", "k"],
+            "k": ["h", "j"],
+        }) == True
+
+    def test_05(self):
+        assert can_color({
+            "z": []
+        }) == True
+
+    def test_06(self):
+        assert can_color({
+            "h": ["i", "k"],
+            "i": ["h", "j"],
+            "j": ["i", "k"],
+            "k": ["h", "j"],
+            "q": ["r", "s"],
+            "r": ["q", "s"],
+            "s": ["r", "q"]
+        }) == False
+
+
+if __name__ == "__main__":
+    unittest.main()
