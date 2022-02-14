19ed5f2d494f60d239e047d9e451ddee050bd492
Siim Veske <siim.veske@gmail.com>
2022-02-14 09:40:47 +0200

    fix tests. replace popping with index

--- 9. Mixed Recall/120_build_tree_in_post.py
+++ 9. Mixed Recall/120_build_tree_in_post.py
@@ -18,12 +18,12 @@ def build_tree_in_post(in_order, post_order):
     if not post_order:
         return None
 
-    value = post_order.pop()
+    value = post_order[-1]
     middle = in_order.index(value)
     left_in = in_order[:middle]
     right_in = in_order[middle + 1:]
     left_post = post_order[:len(left_in)]
-    right_post = post_order[len(left_in):]
+    right_post = post_order[len(left_in):-1]
 
     root = Node(value)
     root.left = build_tree_in_post(left_in, left_post)
@@ -32,59 +32,92 @@ def build_tree_in_post(in_order, post_order):
     return root
 
 
+def post_order_traversal(root, values):
+    if not root:
+        return
+    post_order_traversal(root.left, values)
+    post_order_traversal(root.right, values)
+    values.append(root.val)
+
+    return values
+
+
+def in_order_traversal(root, values):
+    if not root:
+        return
+    in_order_traversal(root.left, values)
+    values.append(root.val)
+    in_order_traversal(root.right, values)
+
+    return values
+
+
 class Test(unittest.TestCase):
-    # def test_00(self):
-    #     result = build_tree_in_post(
-    #         ['y', 'x', 'z'],
-    #         ['y', 'z', 'x']
-    #     )
-    #     print()
-    #     #       x
-    #     #    /    \
-    #     #   y      z
+    def test_00(self):
+        in_order = ['y', 'x', 'z']
+        post_order = ['y', 'z', 'x']
+        result = build_tree_in_post(in_order, post_order)
+
+        #       x
+        #    /    \
+        #   y      z
+
+        assert in_order_traversal(result, []) == in_order
+        assert post_order_traversal(result, []) == post_order
 
     def test_01(self):
-        result = build_tree_in_post(
-            ['d', 'b', 'e', 'a', 'f', 'c', 'g'],
-            ['d', 'e', 'b', 'f', 'g', 'c', 'a']
-        )
-        print()
+        in_order = ['d', 'b', 'e', 'a', 'f', 'c', 'g']
+        post_order = ['d', 'e', 'b', 'f', 'g', 'c', 'a']
+        result = build_tree_in_post(in_order, post_order)
+
         #      a
         #    /    \
         #   b      c
         #  / \    / \
         # d   e  f   g
 
-    # def test_02(self):
-    #     result = build_tree_in_post(
-    #         ['d', 'b', 'g', 'e', 'h', 'a', 'c', 'f'],
-    #         ['d', 'g', 'h', 'e', 'b', 'f', 'c', 'a']
-    #     )
-    #     #      a
-    #     #    /    \
-    #     #   b      c
-    #     #  / \      \
-    #     # d   e      f
-    #     #    / \
-    #     #    g  h
+        assert in_order_traversal(result, []) == in_order
+        assert post_order_traversal(result, []) == post_order
 
-    # def test_03(self):
-    #     result = build_tree_in_post(
-    #         ['m', 'n'],
-    #         ['m', 'n']
-    #     )
-    #     #       n
-    #     #     /
-    #     #    m
+    def test_02(self):
+        in_order = ['d', 'b', 'g', 'e', 'h', 'a', 'c', 'f']
+        post_order = ['d', 'g', 'h', 'e', 'b', 'f', 'c', 'a']
+        result = build_tree_in_post(in_order, post_order)
 
-    # def test_04(self):
-    #     result = build_tree_in_post(
-    #         ['n', 'm'],
-    #         ['m', 'n']
-    #     )
-    #     #     n
-    #     #      \
-    #     #       m
+        #      a
+        #    /    \
+        #   b      c
+        #  / \      \
+        # d   e      f
+        #    / \
+        #    g  h
+
+        assert in_order_traversal(result, []) == in_order
+        assert post_order_traversal(result, []) == post_order
+
+    def test_03(self):
+        in_order = ['m', 'n']
+        post_order = ['m', 'n']
+        result = build_tree_in_post(in_order, post_order)
+
+        #       n
+        #     /
+        #    m
+
+        assert in_order_traversal(result, []) == in_order
+        assert post_order_traversal(result, []) == post_order
+
+    def test_04(self):
+        in_order = ['n', 'm']
+        post_order = ['m', 'n']
+        result = build_tree_in_post(in_order, post_order)
+
+        #     n
+        #      \
+        #       m
+
+        assert in_order_traversal(result, []) == in_order
+        assert post_order_traversal(result, []) == post_order
 
 
 if __name__ == "__main__":
